3596f3bf7ff580313f3c87637ee8cc018c093dd5
RM-WKS-W95\atif.iqbal.butt <atifse19@gmail.com>
2023-09-18 11:59:17 +0500

    project setup

---
+++ news-api/src/main/java/com/orion/NewsDaily/NewsDailyApplication.java
@@ -0,0 +1,13 @@
+package com.orion.NewsDaily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsDailyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsDailyApplication.class, args);
+	}
+
+}
