b89102cb0051d5c5956f776f7e48aafac52e6d2c
RM-WKS-W95\atif.iqbal.butt <atifse19@gmail.com>
2023-09-18 14:09:57 +0500

    some classes added

--- news-api/src/main/java/com/orion/newsdaily/Tag/Tag.java
+++ news-api/src/main/java/com/orion/newsdaily/Tag/Tag.java
@@ -1,4 +1,12 @@
 package com.orion.newsdaily.Tag;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class Tag {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+
 }
