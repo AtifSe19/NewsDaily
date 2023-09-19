028beafd7dbb2b32ae0f546fa421f5de66bdc6d0
RM-WKS-W95\atif.iqbal.butt <atifse19@gmail.com>
2023-09-19 14:17:20 +0500

    comment class in progress

--- news-api/src/main/java/com/orion/newsdaily/comment/CommentRepo.java
+++ news-api/src/main/java/com/orion/newsdaily/comment/CommentRepo.java
@@ -1,4 +1,20 @@
 package com.orion.newsdaily.comment;
 
-public class CommentRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CommentRepo extends JpaRepository <Comment, Long> {
+
+    @Query(value = "SELECT * FROM comments WHERE fk_user_id = ?1 AND status = 'false'", nativeQuery = true)
+    List<Comment> findPendingCommentsByUserId(Long id);
+
+    @Query(value = "SELECT * FROM comments WHERE fk_news_article_id = ?1 AND status = 'true'", nativeQuery = true)
+    List<Comment> NewsSpecificComments(Long id);
+
+    @Query(value = "SELECT * FROM comments WHERE status = 'false'", nativeQuery = true)
+    List<Comment> findPendingComments();
 }
