f7fcdca70577d5a68e40df22464958721c17dd62
RM-WKS-W95\atif.iqbal.butt <atifse19@gmail.com>
2023-09-19 17:12:45 +0500

    simplifying the code

--- news-api/src/main/java/com/orion/newsdaily/newsTag/NewsTagRepo.java
+++ news-api/src/main/java/com/orion/newsdaily/newsTag/NewsTagRepo.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface NewsTagRepo extends JpaRepository<NewsTag, Long> {
-
     @Query(value = "SELECT * FROM NEWS_TAGS WHERE NEWS_ARTICLE_ID = ?1 AND TAG_ID = ?2", nativeQuery = true)
     NewsTag getIdByNTID(Long nId, Long tId);
 }
