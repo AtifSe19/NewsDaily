fc7638455414066abd7d41a262d3605bd49efe55
RM-WKS-W94\hassan.raza <hassan.raza@gmail.com>
2023-09-20 12:34:17 +0500

    check

--- news-api/src/main/java/com/orion/newsdaily/newsArticle/NewsArticleRepo.java
+++ news-api/src/main/java/com/orion/newsdaily/newsArticle/NewsArticleRepo.java
@@ -11,7 +11,7 @@ import java.util.List;
 public interface NewsArticleRepo extends JpaRepository<NewsArticle, Long > {
     @Query(value = "SELECT * FROM news_articles WHERE is_approved = false", nativeQuery = true)
     List<NewsArticle> findPendingNews();
-    @Query(value = "SELECT * FROM news_articles WHERE is_approved = true AND is_disabled = false", nativeQuery = true)
+    @Query(value = "SELECT * FROM news_articles WHERE is_approved = true AND is_disabled = false order by POSTED_AT DESC", nativeQuery = true)
     List<NewsArticle> findAllNews();
 
     @Query(value = "SELECT * FROM news_articles WHERE is_approved = true AND is_disabled = false AND is_sponsored = false", nativeQuery = true)
