f8c6730ba0ef7341ebe0623453b6c0ab4c12317b
RM-WKS-W95\atif.iqbal.butt <atifse19@gmail.com>
2023-09-22 12:00:20 +0500

    For editor role: only disable functionality is yet to be completed

--- news-ui/src/App.js
+++ news-ui/src/App.js
@@ -4,15 +4,15 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import Navbar from './components/navbar/Navbar';
-import User from './pages/user/userDashboard/User'
+import UserDashboard from './pages/user/userDashboard/UserDashboard'
 import './App.css';
 
 function App() {
   return (
     <Router>
       <Navbar />
-      {/* <UserDashboard /> */}
-      <User/>
+      <UserDashboard />
+      {/* <User/> */}
     </Router>
   );
 }
