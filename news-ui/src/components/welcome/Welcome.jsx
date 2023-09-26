ee7d3a72b2edbccc886cbc34e094913dd4b81157
DESKTOP-3D9VEJF\muttahir.islam <muttahirislam111.mi@gmail.com>
2023-09-26 14:34:27 +0500

    Dynamic Welcome page

--- news-ui/src/components/welcome/Welcome.jsx
+++ news-ui/src/components/welcome/Welcome.jsx
@@ -1,35 +1,53 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-
 import { toast } from "react-toastify";
 
 const Welcome = user => {
   const [username, setUsername] = useState(null);
   const [text1, setText1] = useState("");
   const [text2, setText2] = useState("");
-
+  const [error, setError] = useState(null); 
+  const styles1 = {
+    fontSize: '36px',
+    marginBottom: '20px',
+    fontWeight: 'bold', 
+    color: '#1E5939', 
+    fontFamily: 'Arial, sans-serif' 
+  };
+  const styles2 = {
+    fontSize: '18px', 
+    color: '#0F2D1D', 
+    fontFamily: 'Arial, sans-serif' 
+  };
+  
   const handleWelcome = () => {
     toast.success(`Welcome, @_${username}! to the ${user.role} dashboard`, {
       position: toast.POSITION.TOP_CENTER
     });
   };
-  const fullText1 = <h1>Welcome, @_{username}!</h1>;
-  const fullText2 = "This is the {user.role} dashboard of the NewsDaily Web Application.";
-  const styles1 = {
-    fontSize: '36px',
-    marginBottom: '20px',
-    fontWeight: 'bold', 
-    color: '#0074d9', 
-    fontFamily: 'Arial, sans-serif' 
+  const fetchData = async () => {
+    try {
+      const usernameResponse = await axios.get(`/api/v1/users/getUsername`);
+      if (usernameResponse.status === 200) {
+        const username = usernameResponse.data;
+        setUsername(username);
+      } else {
+        setError("Failed to fetch username");
+      }
+    } catch (error) {
+      setError("Error: " + error.message);
+    }
   };
-  const styles2 = {
-    fontSize: '18px', 
-    color: 'white', 
-    fontFamily: 'Arial, sans-serif' 
-  };
-  useEffect(() => {
-    let currentIndex1 = 0;
 
+  useEffect(() => {
+    fetchData(); 
+  }, []);
+
+  useEffect(() => {
+    const fullText1 = username ? `Welcome, @_${username}!` : ""; 
+    const fullText2 = `This is the ${user.role} dashboard of the NewsDaily Web Application.`;
+
+    let currentIndex1 = 0;
     const interval1 = setInterval(() => {
       if (currentIndex1 <= fullText1.length) {
         setText1(fullText1.slice(0, currentIndex1));
@@ -50,39 +68,25 @@ const Welcome = user => {
     }, 100);
 
     return () => clearInterval(interval1);
-  }, []);
+  }, [username, user.role]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const usernameResponse = await axios.get(`/api/v1/users/getUsername`);
-
-        if (usernameResponse.status === 200) {
-          const username = usernameResponse.data;
-          setUsername(username);
-        } else {
-          console.error("Failed to fetch username");
-        }
-      } catch (error) {
-        console.error("Error:", error);
-      }
-    };
-
-    fetchData();
-  }, []);
   return (
     <div className="container mt-5">
       <div className="row">
         <div className="col-md-6 offset-md-3 text-center">
           <h1 style={styles1}>{text1}</h1>
           <p style={styles2}>{text2}</p>
-          <button
-            className="btn btn-primary"
-            onClick={handleWelcome}
-            style={{ border: "1px solid #008080" }}
-          >
-            Welcome!
-          </button>
+          {error ? (
+            <p>Error: {error}</p>
+          ) : (
+            <button
+              className="btn btn-primary"
+              onClick={handleWelcome}
+              style={{ border: "1px solid #008080" }}
+            >
+              Welcome!
+            </button>
+          )}
         </div>
       </div>
     </div>
