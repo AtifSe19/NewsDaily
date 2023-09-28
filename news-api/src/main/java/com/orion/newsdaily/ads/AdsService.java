85967126c263bf37736a78d2070a17ceccd3b5cd
DESKTOP-3D9VEJF\muttahir.islam <muttahirislam111.mi@gmail.com>
2023-09-28 11:59:23 +0500

    add implementation

--- news-api/src/main/java/com/orion/newsdaily/ads/AdsService.java
+++ news-api/src/main/java/com/orion/newsdaily/ads/AdsService.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -41,8 +42,16 @@ public class AdsService {
     }
 
 
+//    public List<NewsArticle> findMyAds() {
+//        return getAdsByTagList(auditTrailService.getUserPreferencesByUserId(userService.getAuth()));
+//    }
     public List<NewsArticle> findMyAds() {
-//        List<Long> list = auditTrailService.getUserPreferencesByUserId(auth);
-        return getAdsByTagList(auditTrailService.getUserPreferencesByUserId(userService.getAuth()));
+        List<NewsArticle> adsList = getAdsByTagList(auditTrailService.getUserPreferencesByUserId(userService.getAuth()));
+        Collections.shuffle(adsList);
+        if (!adsList.isEmpty()) {
+            return Collections.singletonList(adsList.get(0));
+        } else {
+            return Collections.emptyList();
+        }
     }
 }
--- news-ui/src/pages/user/newsList/NewsList.jsx
+++ news-ui/src/pages/user/newsList/NewsList.jsx
@@ -157,7 +157,7 @@ const NewsList = () => {
           profileImageSrc="https://images.unsplash.com/photo-1554780336-390462301acf?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
           author={article.reporter}
           followers={article.postedAt.slice(0, 10)}
-          id={article.id} // Pass the news article's id
+          id={article.id} 
           openModal={openModal}
         />
       ))}
